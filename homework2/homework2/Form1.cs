645fad14ead63fc464d2ed79496955f3da176b45
Shahriyor <94789479+shahriyorreal@users.noreply.github.com>
2021-11-26 15:08:36 +0500

    task1

---
+++ homework2/homework2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int week = int.Parse(textBox1.Text);
+            int day = week * 7;
+            label1.Text = day.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int year = int.Parse(textBox1.Text);
+            int day = year * 365;
+            label1.Text = day.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int mounth = int.Parse(textBox1.Text);
+            int day = mounth * 30;
+            label1.Text = day.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
